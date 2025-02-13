cb6d50ee3e55848d94f8c063f7472bb11f9c2ba7
MMAmunzir <munzirmuneer1998@gamil.com>
2025-02-13 08:07:54 -0500

    task completed

---
+++ calculateArea/groceryTracker.js
@@ -0,0 +1,12 @@
+let amount1
+let amount2
+let amount3
+
+function calculateAmount(){
+    amount1 = parseFloat(document.getElementById('amount1').value)
+    amount2 = parseFloat(document.getElementById('amount2').value)
+    amount3 = parseFloat(document.getElementById('amount3').value)
+    let sum = amount1 + amount2 + amount3
+
+    document.getElementById('result').innerText=`total amoun:${sum}`
+}
